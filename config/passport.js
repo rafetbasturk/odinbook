91a9769e7b327521209db1a69d7a4cfce451dbe3
Rafet <rafetbasturk@yahoo.com>
2023-05-02 19:12:16 +0300

    updated readme

--- config/passport.js
+++ config/passport.js
@@ -15,6 +15,7 @@ passport.use(
   new FacebookStrategy({
     clientID: process.env.FACEBOOK_APP_ID,
     clientSecret: process.env.FACEBOOK_APP_SECRET,
+    // callbackURL: "/api/v1/auth/facebook/callback",
     callbackURL: "https://rafet-odinbook.onrender.com/api/v1/auth/facebook/callback",
     profileFields: ['id', 'displayName', 'name', 'gender', 'picture.type(large)', 'email'],
     session: false
